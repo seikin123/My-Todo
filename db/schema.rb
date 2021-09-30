0d983973297867f04a8fbcbb644e6492dfebb19d
ヨシダタイキ <abc521fjr7@gmail.com>
2021-09-30 15:38:01 +0900

    [add]退会カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_28_042610) do
+ActiveRecord::Schema.define(version: 2021_09_29_122853) do
 
   create_table "cards", force: :cascade do |t|
     t.string "title", limit: 255, null: false
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2021_09_28_042610) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name", default: "", null: false
+    t.boolean "is_deleted", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
