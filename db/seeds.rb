cd04900282b31a238f4462016c4b7a490224f81a
ヨシダタイキ <abc521fjr7@gmail.com>
2021-10-04 17:54:46 +0900

    [add]seedデータ変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,48 +10,19 @@ List.create!(
   [
     {
       title: 'ToDo',
-      user_id: 5,
+      user_id: 1,
     },
     {
       title: 'in Progress',
-      user_id: 5,
+      user_id: 1,
     },
     {
       title: 'Done',
-      user_id: 5,
+      user_id: 1,
     },
     {
       title: 'Archive',
-      user_id: 5,
-    }
-  ]
-)
-
-Card.create!(
-  [
-    {
-      title: 'Reactを学ぶ',
-      memo: '',
-      list_id: 17,
-      deadline: "2022-2-05 09:00:00"
-    },
-    {
-      title: 'Dockerの勉強',
-      memo: '',
-      list_id: 18,
-      deadline: "2022-2-05 09:00:00"
-    },
-    {
-      title: 'ご飯を食べる',
-      memo: 'ラーメンを食べよう',
-      list_id: 19,
-      deadline: "2022-2-05 09:00:00"
-    },
-    {
-      title: 'Rubyのクラスについて学ぶ',
-      memo: 'ドットインストール',
-      list_id: 20,
-      deadline: "2022-2-05 09:00:00"
+      user_id: 1,
     }
   ]
 )
