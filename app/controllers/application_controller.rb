e4eee0e2f33b70e687030417562eefa01e991db6
ヨシダタイキ <abc521fjr7@gmail.com>
2021-10-05 04:34:43 +0900

    [add]systemテスト追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
 
+  protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
       devise_parameter_sanitizer.permit(:account_update, keys: [:name])
---
+++ spec/system/01_before_login_spec.rb
@@ -0,0 +1,183 @@
+require 'rails_helper'
+
+  describe 'ヘッダーのテスト: ログインしていない場合' do
+    before do
+      visit root_path
+    end
+
+    context '表示内容の確認' do
+      it 'My-ToDoリンクが表示される: 左上から1番目のリンクが「My-ToDo」である' do
+        root_link = find_all('a')[1].native.inner_text
+        expect(recipes_link).to match(//i)
+      end
+    end
+
+    context 'リンクの内容を確認' do
+      subject { current_path }
+
+      it 'loginを押すと、ログイン画面に遷移する' do
+        login_link = find_all('a')[3].native.inner_text
+        login_link = login_link.gsub(/\n/, '').gsub(/\A\s*/, '').gsub(/\s*\Z/, '')
+        click_link login_link
+        is_expected.to eq '/users/sign_in'
+      end
+      it 'sign upを押すと、新規登録画面に遷移する' do
+        signup_link = find_all('a')[4].native.inner_text
+        signup_link = signup_link.gsub(/\n/, '').gsub(/\A\s*/, '').gsub(/\s*\Z/, '')
+        click_link signup_link
+        is_expected.to eq '/users/sign_up'
+      end
+    end
+  end
+
+  describe 'ユーザ新規登録のテスト' do
+    before do
+      visit new_user_registration_path
+    end
+
+    context '表示内容の確認' do
+      it 'URLが正しい' do
+        expect(current_path).to eq '/users/sign_up'
+      end
+      it '「アカウントを新規作成」と表示される' do
+        expect(page).to have_content 'Sign up'
+      end
+      it 'nameフォームが表示される' do
+        expect(page).to have_field 'user[name]'
+      end
+      it 'emailフォームが表示される' do
+        expect(page).to have_field 'user[email]'
+      end
+      it 'passwordフォームが表示される' do
+        expect(page).to have_field 'user[password]'
+      end
+      it 'password_confirmationフォームが表示される' do
+        expect(page).to have_field 'user[password_confirmation]'
+      end
+      it 'アカウントを作成ボタンが表示される' do
+        expect(page).to have_button 'Sign up'
+      end
+    end
+
+    context '新規登録成功のテスト' do
+      before do
+        fill_in 'user[name]', with: Faker::Lorem.characters(number: 10)
+        fill_in 'user[email]', with: Faker::Internet.email
+        fill_in 'user[password]', with: 'password'
+        fill_in 'user[password_confirmation]', with: 'password'
+      end
+
+      it '正しく新規登録される' do
+        expect { click_button 'Sign up' }.to change(User.all, :count).by(1)
+      end
+      it '新規登録後のリダイレクト先が、root_pathになっている' do
+        click_button 'Sign up'
+        expect(root_path).to eq '/'
+      end
+    end
+  end
+
+  describe 'ユーザログイン' do
+    let(:user) { create(:user) }
+
+    before do
+      visit new_user_session_path
+    end
+
+    context '表示内容の確認' do
+      it 'URLが正しい' do
+        expect(current_path).to eq '/users/sign_in'
+      end
+      it '「My-ToDoにサインイン」と表示される' do
+        expect(page).to have_content 'Log in'
+      end
+      it 'nameフォームが表示される' do
+        expect(page).to have_field 'user[name]'
+      end
+      it 'passwordフォームが表示される' do
+        expect(page).to have_field 'user[password]'
+      end
+      it 'サインインボタンが表示される' do
+        expect(page).to have_button 'Log in'
+      end
+      it 'emailフォームは表示されない' do
+        expect(page).not_to have_field 'user[email]'
+      end
+    end
+
+    context 'ログイン成功のテスト' do
+      before do
+        fill_in 'user[name]', with: user.name
+        fill_in 'user[password]', with: user.password
+        click_button 'Log in'
+      end
+
+      it 'ログイン後のリダイレクト先が、ログインした人のマイページになっている' do
+        expect(current_path).to eq '/use/i' + user.id.to_s
+      end
+    end
+
+    context 'ログイン失敗のテスト' do
+      before do
+        fill_in 'user[name]', with: ''
+        fill_in 'user[password]', with: ''
+        click_button 'サインイン'
+      end
+
+      it 'ログインに失敗し、ログイン画面にリダイレクトされる' do
+        expect(current_path).to eq '/users/sign_in'
+      end
+    end
+  end
+
+  describe 'ヘッダーのテスト: ログインしている場合' do
+    let(:user) { create(:user) }
+
+    before do
+      visit new_user_session_path
+      fill_in 'user[name]', with: user.name
+      fill_in 'user[password]', with: user.password
+      click_button 'サインイン'
+    end
+
+    context 'ヘッダーの表示を確認' do
+
+      it 'My-ToDoリンクが表示される: 左上から1番目のリンクが「root_path」である' do
+        root_link = find_all('a')[1].native.inner_text
+        expect(root_link).to match(/)
+      end
+      it 'リスト作成リンクが表示される: 左上から2番目のリンクが「リスト作成」である' do
+        new_list_link = find_all('a')[2].native.inner_text
+        expect(new_list_link).to match(/list/new)
+      end
+      it 'マイページリンクが表示される: 左上から3番目のリンクが「マイページ」である' do
+        user_link = find_all('a')[3].native.inner_text
+        expect(user_link).to match(/user/i)
+      end
+      it 'サインアウトリンクが表示される: 左上から4番目のリンクが「サインアウト 」である' do
+        signout_link = find_all('a')[4].native.inner_text
+        expect(signout_link).to match(/sign out/i)
+      end
+    end
+  end
+
+  describe 'ユーザログアウトのテスト' do
+    let(:user) { create(:user) }
+
+    before do
+      visit new_user_session_path
+      fill_in 'user[name]', with: user.name
+      fill_in 'user[password]', with: user.password
+      click_button 'Log in'
+      logout_link = find_all('a')[4].native.inner_text
+      logout_link = logout_link.gsub(/\n/, '').gsub(/\A\s*/, '').gsub(/\s*\Z/, '')
+      click_link logout_link
+    end
+
+    context 'ログアウト機能のテスト' do
+      it 'ログアウト後のリダイレクト先が、トップになっている' do
+        expect(current_path).to eq '/'
+      end
+    end
+  end
+end
